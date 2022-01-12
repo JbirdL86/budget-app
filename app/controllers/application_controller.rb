96d098ecd646bbcf30aac4deca1c38357e6aa642
JbirdL86 <jgonzalez.wj@gmail.com>
2022-01-12 11:03:04 -0600

    Add devise for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name, email, password, password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :surname, :email, :password, :current_password)
+    end
+  end
+
 end
+# class ApplicationController < ActionController::Base
+#   protect_from_forgery with: :null_session
+#   before_action :update_allowed_parameters, if: :devise_controller?
+#   before_action :authenticate_user!
+  
+#   protected
+
+#   def update_allowed_parameters
+#     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+#      devise_parameter_sanitizer.permit(:account_update) do |u|
+#       u.permit(:name, :surname, :email, :password, :current_password)
+#      end
+#   end
+# end
